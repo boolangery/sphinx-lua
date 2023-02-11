85f68544d10079f885f9b6d431fa3b8bf91d4a0e
Eliott Dumeix <eliott.dumeix@gmail.com>
2023-02-11 20:31:07 +0100

    bump version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sphinx-lua',
-    version='1.1.3',
+    version='1.1.4',
     description='Support for using Sphinx on Luadoc-documented Lua code',
     long_description=open('README.rst', 'r', encoding='utf8').read(),
     author='Eliott Dumeix',
