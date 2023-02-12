c7fb743bda7112f7c5be5b5aa7647f65ca89804e
Eliott Dumeix <eliott.dumeix@gmail.com>
2023-02-12 11:53:49 +0100

    fix bump version

--- bump-version.sh
+++ bump-version.sh
@@ -2,8 +2,8 @@
 
 version=$1
 sed -i -e "s/^\([[:blank:]]*__version__ = \).*/\1'$version'/" sphinx_lua/version.py
-git add -A
-git command  -m "bump version to $version"
+git add sphinx_lua/version.py
+git commit  -m "bump version to $version"
 git tag $version
 git push
 git push origin $version
